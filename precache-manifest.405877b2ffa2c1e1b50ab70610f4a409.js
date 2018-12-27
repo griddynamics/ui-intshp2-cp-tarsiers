9ee08f5afdd14015b22ae638d3bda504be27677a
Prezlyata Yuriy <Prezlyata Yuriy>
2018-12-27 14:55:43 +0200

    Updates

---
+++ precache-manifest.405877b2ffa2c1e1b50ab70610f4a409.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/ui-intshp2-cp-tarsiers/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "eaec6d0f14323506046d",
+    "url": "/ui-intshp2-cp-tarsiers/static/js/runtime~main.eaec6d0f.js"
+  },
+  {
+    "revision": "4fce4f0c894599bc771e",
+    "url": "/ui-intshp2-cp-tarsiers/static/js/main.4fce4f0c.chunk.js"
+  },
+  {
+    "revision": "0a7a1053aaedd785abb5",
+    "url": "/ui-intshp2-cp-tarsiers/static/js/1.0a7a1053.chunk.js"
+  },
+  {
+    "revision": "4fce4f0c894599bc771e",
+    "url": "/ui-intshp2-cp-tarsiers/static/css/main.5a921b5c.chunk.css"
+  },
+  {
+    "revision": "609ef8491017d26577f0edf31ca4b9a7",
+    "url": "/ui-intshp2-cp-tarsiers/index.html"
+  }
+];
