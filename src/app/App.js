98a1c69c58e941aa92f6f541ae4fff61118b4da0
Prezlyata Yuriy <Prezlyata Yuriy>
2018-12-27 14:53:29 +0200

    initial commit

--- src/app/App.js
+++ src/app/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import logo from '../assets/logo.svg';
+import './App.scss';
 
 class App extends Component {
   render() {
@@ -9,16 +9,8 @@ class App extends Component {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-            Edit <code>src/App.js</code> and save to reload.
+            Tarsiers
           </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
         </header>
       </div>
     );
---
+++ src/app/components/Counter/CounterContainer.js
@@ -0,0 +1,12 @@
+import { connect } from 'react-redux';
+import Counter from './Counter';
+
+const mapStateToProps = state => ({
+  counter: state.counter.value
+});
+
+const CounterContainer = connect(
+  mapStateToProps
+)(Counter);
+
+export default CounterContainer;
