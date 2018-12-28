d17e4cc5408ecd22b0a25150183e0fbdd7dda829
Sophia Matviishyn <sofiyamatviishyn@gmail.com>
2018-12-28 13:10:58 +0200

    [Sofia]Feature/uio 2 (#3)
    * UIO2/added prettier, prerush hooks, disabled useless eslint rules
    
    * UIO2/added prettierrc
    
    * UIO2/fixed problem with arrow parens
    
    * UIO2/disabled rule arrow-parens
    
    * UIO2/added package-lock.json to .gitignore
    
    * UIO2/fixed some eslint rules
    
    * UIO2/changed prepush hooks
    
    * UIO2/changed prepush & precommit
    
    * UIO2/changed prepush & precommit 2.0
    
    * UIO2/changed prepush & precommit 3.0
    
    * UIO2/deleted package-lock.json
    
    * UIO2/deleted .rej files

--- src/app/App.js
+++ src/app/App.js
@@ -1,20 +1,14 @@
-import React, { Component } from 'react';
+import React from 'react';
 import logo from '../assets/logo.svg';
 import './App.scss';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Tarsiers
-          </p>
-        </header>
-      </div>
-    );
-  }
-}
+const App = () => (
+  <div className="App">
+    <header className="App-header">
+      <img src={logo} className="App-logo" alt="logo" />
+      <p>Tarsiers</p>
+    </header>
+  </div>
+);
 
 export default App;
--- src/app/components/Counter/Counter.js
+++ src/app/components/Counter/Counter.js
@@ -1,7 +1,6 @@
 import React from 'react';
 
 export default (props) => {
-  return (
-    <div className="Counter">{props.counter}</div>
-  );
-}
+  const { counter } = props;
+  return <div className="Counter">{counter}</div>;
+};
