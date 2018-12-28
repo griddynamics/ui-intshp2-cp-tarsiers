c7f28dc1e0a1faab83c938daaeab31783f3819f9
Andriy Zhukevych <reactive.adnrew@yahoo.com.ua>
2018-12-28 16:32:50 +0200

    UIO-5/ added header and footer

--- src/app/components/Footer/Footer.js
+++ src/app/components/Footer/Footer.js
@@ -1,7 +1,55 @@
-import React from "react";
+import React from 'react';
+import logo from '../../../assets/logo.png';
 
-const Footer = (props) => {
-    return (props.visible) ? <footer className ="jc-center flx"><h2>footer</h2></footer> : null;
-}
+const Footer = props => {
+  const { visible } = props;
+  return visible ? (
+    <footer className="footer">
+      <nav className="footer__block mt-60 container">
+        <div className="footer__copyright col-3">
+          <img src={logo} alt="" />
+          <span>Shopy @copyright 2018</span>
+        </div>
+        <div className="footer__links col-7">
+          <ul>
+            <li>
+              <a href="/" className="active">
+                about us
+              </a>
+            </li>
+            <li>
+              <a href="/">contact us</a>
+            </li>
+            <li>
+              <a href="/">support</a>
+            </li>
+          </ul>
+          <ul>
+            <li>
+              <a href="/">our feed</a>
+            </li>
+            <li>
+              <a href="/">terms and conditions</a>
+            </li>
+            <li>
+              <a href="/">our privacy</a>
+            </li>
+          </ul>
+          <ul>
+            <li>
+              <a href="/">join us</a>
+            </li>
+            <li>
+              <a href="/">live support</a>
+            </li>
+          </ul>
+        </div>
+        <div className="footer__payments col-2">
+          <p className="active">Payment Methods</p>
+        </div>
+      </nav>
+    </footer>
+  ) : null;
+};
 
 export default Footer;
