6b21d0d227c91b982b95c34ba4d1aec2eab69a17
EdGrLab <grabchak1999@gmail.com>
2019-01-02 18:39:36 +0200

    UIO-8/ fixed HeaderTop component

--- src/app/components/Header/HeaderTop.js
+++ src/app/components/Header/HeaderTop.js
@@ -1,22 +1,20 @@
 import React from 'react';
 
-const HeaderTop = props => {
-  return (
-    <div className="header-top">
-      <i className="far fa-envelope">
-        <span>info@shopy.com</span>
-      </i>
-      <i className="fas fa-book">
-        <span>453 - 5553 - 996</span>
-      </i>
-      <div className="icons-right">
-        <i className="fab fa-facebook-f" />
-        <i className="fab fa-twitter" />
-        <i className="fab fa-google-plus" />
-        <i className="fab fa-instagram" />
-      </div>
+const HeaderTop = () => (
+  <div className="header-top">
+    <i className="far fa-envelope">
+      <span>info@shopy.com</span>
+    </i>
+    <i className="fas fa-book">
+      <span>453 - 5553 - 996</span>
+    </i>
+    <div className="icons-right">
+      <i className="fab fa-facebook-f" />
+      <i className="fab fa-twitter" />
+      <i className="fab fa-google-plus" />
+      <i className="fab fa-instagram" />
     </div>
-  );
-};
+  </div>
+);
 
 export default HeaderTop;
