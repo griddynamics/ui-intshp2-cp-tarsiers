9c98d1500cf8cf501ad06e4e89c968baa95c8ac5
EdGrLab <grabchak1999@gmail.com>
2019-01-04 13:54:05 +0200

    fixed navbar, used obj destruction

--- src/app/components/Header/Header.js
+++ src/app/components/Header/Header.js
@@ -4,72 +4,80 @@ import { Link } from 'react-router-dom';
 import logo from '../../../assets/logo.png';
 import HeaderTop from './HeaderTop';
 
-const Header = props => {
-  const { headerFooterVisibility } = props;
-  const visible = headerFooterVisibility.value;
-  const navLinks = [
-    { id: 0, val: 'Home', link: '/' },
-    { id: 1, val: 'Products', link: '/products' },
-    { id: 2, val: 'Hot Deals', link: '/hot-deals' },
-    { id: 3, val: 'About', link: '/about' },
-    { id: 4, val: 'Contact', link: '/contacts' }
-  ];
-
-  const navItems = navLinks.map(item => (
-    <li key={item.id} className="navbar-link">
-      <Link to={item.link}>{item.val}</Link>
-    </li>
-  ));
-
-  const navToggle = () => {
-    const navMain = document.querySelector('.nav-main');
-    navMain.classList.toggle('nav-toggle');
+class Header extends React.Component {
+  state = {
+    isHidden: true
   };
 
-  return visible ? (
-    <header className="header">
-      <div className="container">
-        <HeaderTop />
-        <div className="header-main">
-          <div className="col-2">
-            <Link to="/">
-              <img src={logo} alt="logo" className="logo" />
-            </Link>
-          </div>
-          <nav className="navbar col-10">
-            <div className="nav-main">
-              <ul className="header-links">{navItems}</ul>
+  navToggle = () => {
+    this.setState(prevState => ({ isHidden: !prevState.isHidden }));
+  };
+
+  render() {
+    const { isHidden } = this.state;
+    const { headerFooterVisibility } = this.props;
+    const visible = headerFooterVisibility.value;
+    const navLinks = [
+      { id: 0, val: 'Home', link: '/' },
+      { id: 1, val: 'Products', link: '/products' },
+      { id: 2, val: 'Hot Deals', link: '/hot-deals' },
+      { id: 3, val: 'About', link: '/about' },
+      { id: 4, val: 'Contact', link: '/contacts' }
+    ];
+
+    const navItems = navLinks.map(item => {
+      const { id, link, val } = item;
+
+      return (
+        <li key={id} className="navbar-link">
+          <Link to={link}>{val}</Link>
+        </li>
+      );
+    });
+
+    return visible ? (
+      <header className="header">
+        <div className="container">
+          <HeaderTop />
+          <div className="header-main">
+            <div className="col-2">
+              <Link to="/">
+                <img src={logo} alt="logo" className="logo" />
+              </Link>
             </div>
-            <div className="header-nav-right col-3">
-              <ul className="header-links">
-                <li className="navbar-link">
-                  <a href="/">
-                    <i className="fa fa-search" />
-                  </a>
-                </li>
-                <li className="navbar-link">
-                  <a href="/">
-                    <i className="fa fa-user" />
-                  </a>
-                </li>
-                <li className="navbar-link">
-                  <a href="/">
-                    <i className="fas fa-shopping-basket" />
-                  </a>
-                </li>
-                <li className="navbar-link">
-                  <button type="button" className="menu" onClick={navToggle}>
+            <nav className="navbar col-10">
+              <div className={!isHidden ? 'nav-main' : 'nav-toggle'}>
+                <ul className="header-links">{navItems}</ul>
+              </div>
+              <div className="header-nav-right col-3">
+                <ul className="header-links">
+                  <li className="navbar-link">
+                    <a href="/">
+                      <i className="fa fa-search" />
+                    </a>
+                  </li>
+                  <li className="navbar-link">
+                    <a href="/">
+                      <i className="fa fa-user" />
+                    </a>
+                  </li>
+                  <li className="navbar-link">
+                    <a href="/">
+                      <i className="fas fa-shopping-basket" />
+                    </a>
+                  </li>
+                  <button type="button" className="menu" onClick={this.navToggle}>
                     <i className="fas fa-bars" />
                   </button>
-                </li>
-              </ul>
-            </div>
-          </nav>
+                </ul>
+              </div>
+            </nav>
+          </div>
         </div>
-      </div>
-    </header>
-  ) : null;
-};
+      </header>
+    ) : null;
+  }
+}
 
 const mapStateToProps = state => ({ ...state });
 
